5ea84d9d9dba5cdcbde2fdfebf4119c37262dd07
CamilaAchuri <camilaachury.4@gmail.com>
2023-08-14 14:15:25 -0300

    call api data

--- R/api_data.R
+++ R/api_data.R
@@ -1,6 +1,10 @@
 
 
-api_data <- function(){
+api_data <- function(url){
   #https://cms.flip.datasketch.co/api/periodistas-asesinados
+  respose <- GET( url )
+  result <- fromJSON(content(respose, as = 'text'))
+  result$fecha_agresion <- lubridate::ymd(result$fecha_agresion)
+  result
 }
 
